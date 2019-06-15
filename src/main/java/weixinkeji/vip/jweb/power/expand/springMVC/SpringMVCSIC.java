1d284fc4e933ea48283169ee7447791108122cca
wangchunzi <pankeng1988w@aliyun.com>
2019-06-15 18:50:17 +0800

    2.0

--- src/main/java/weixinkeji/vip/jweb/power/expand/springMVC/SpringMVCSIC.java
+++ src/main/java/weixinkeji/vip/jweb/power/expand/springMVC/SpringMVCSIC.java
@@ -2,6 +2,7 @@ package weixinkeji.vip.jweb.power.expand.springMVC;
 
 import java.lang.reflect.Method;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -20,174 +21,179 @@ import weixinkeji.vip.jweb.power.config.JWPSystemInterfaceConfig;
  * @author wangchunzi
  *
  */
-public class SpringMVCSIC implements JWPSystemInterfaceConfig{
-	
+public class SpringMVCSIC implements JWPSystemInterfaceConfig {
+
 	/**
-	 * 取得注解在类上的路径（有可能是约定方式，url即类名）
+	 * 获取类绑定的URL地址
+	 * <p>
+	 * 此类不是控制类，请返回null
+	 * <p>
+	 * 如果此类没有绑定URL，但是方法绑定了，请返回空
+	 * 
+	 * @param c 绑定了相关注解的类
+	 * @return String
 	 */
 	@Override
 	public String getURLByClass(Class<?> c) {
-		return getUrlMapping(c);
-	}
-	
-	/**
-	 * 取得注解在方法的路径（有可能是约定方式，url即方法名）
-	 */
-	@Override
-	public String getURLByMethod(Method method) {
-		return getUrlMapping(method);
-	}
-	
-	/**
-	 * 请求路径的后缀。
-	 */
-	@Override
-	public String[] getRequestUrlSuffix() {
-		return new String[] { ".json", ".xml" };
-	}
-	//取得SpringMVC 注解在类上的路径
-	private String getUrlMapping(Class<?> c) {
 		RequestMapping request = c.getAnnotation(RequestMapping.class);
 		GetMapping get = c.getAnnotation(GetMapping.class);
 		PostMapping post = c.getAnnotation(PostMapping.class);
 		PutMapping put = c.getAnnotation(PutMapping.class);
 		DeleteMapping delete = c.getAnnotation(DeleteMapping.class);
 		PatchMapping patch = c.getAnnotation(PatchMapping.class);
-		return this.getSpringMVCUrl(request, get, post, put, delete, patch);
+		Controller controller=c.getAnnotation(Controller.class);
+		
+		
+		if (null != request) {
+			return this.getUrlByMapping(request);
+		} else if (null != get) {
+			return this.getUrlByMapping(get);
+		} else if (null != post) {
+			return this.getUrlByMapping(post);
+		} else if (null != put) {
+			return this.getUrlByMapping(put);
+		} else if (null != delete) {
+			return this.getUrlByMapping(delete);
+		} else if (null != patch) {
+			return this.getUrlByMapping(patch);
+		}else if(null!=controller) {//没注有路径。但确实是控制类
+			return "";
+		}
+		return null;
 	}
-	//取得SpringMVC 注解在方法上的路径
-	private String getUrlMapping(Method method) {
+	/**
+	 * 获取方法绑定的URL地址
+	 * 
+	 * <p>
+	 * 此方法不是控制方法，请返回null
+	 * 
+	 * @param method 绑定了相关url的方法
+	 * @return String
+	 */
+	@Override
+	public String getURLByMethod(Method method) {
 		RequestMapping request = method.getAnnotation(RequestMapping.class);
 		GetMapping get = method.getAnnotation(GetMapping.class);
 		PostMapping post = method.getAnnotation(PostMapping.class);
 		PutMapping put = method.getAnnotation(PutMapping.class);
 		DeleteMapping delete = method.getAnnotation(DeleteMapping.class);
 		PatchMapping patch = method.getAnnotation(PatchMapping.class);
-		return this.getSpringMVCUrl(request, get, post, put, delete, patch);
-	}
-
-	//取得SpringMVC 注解在类或方法上的路径
-	private String getSpringMVCUrl(RequestMapping request, GetMapping get, PostMapping post, PutMapping put,
-			DeleteMapping delete, PatchMapping patch) {
-		String url = null;
 		if (null != request) {
-			url = this.getUrlByMapping(request);
+			return this.getUrlByMapping(request);
 		} else if (null != get) {
-			url = this.getUrlByMapping(get);
+			return this.getUrlByMapping(get);
 		} else if (null != post) {
-			url = this.getUrlByMapping(post);
+			return this.getUrlByMapping(post);
 		} else if (null != put) {
-			url = this.getUrlByMapping(put);
+			return this.getUrlByMapping(put);
 		} else if (null != delete) {
-			url = this.getUrlByMapping(delete);
+			return this.getUrlByMapping(delete);
 		} else if (null != patch) {
-			url = this.getUrlByMapping(patch);
+			return this.getUrlByMapping(patch);
 		}
-		return this.toFormatUrl(url);
+		return null;
 	}
 
 	/**
-	 * 变成以/开头的路径
+	 * 请求路径 默认后缀
+	 * <p>
+	 * 有些特别的框架，比如SpringMVC,他配置拦截的后缀没有。
+	 * <p>
+	 * 但针对指定数据格式请求的url却有后缀。比如.json后缀，.xml后缀（即，不是静态资源，却加了静态资源的后缀）
 	 * 
-	 * @param url
-	 * @return
+	 * @return String[]
 	 */
-	private String toFormatUrl(String url) {
-		if (null == url) {
-			return null;
+	@Override
+	public String[] getRequestUrlSuffix() {
+		return new String[] { ".json", ".xml" };
+	}
+	
+	//从 注解PatchMapping 取得路径。并确保它们是/开头（没有，则强加）
+	private String getUrlByMapping(PatchMapping a) {
+		if (null != a.name() && !a.name().isEmpty()) {
+			return formatUrl(a.name());
 		}
-		url = url.trim();
-		if (url.isEmpty()) {
-			return "";
+		if (a.value().length > 0 && !a.value()[0].isEmpty()) {
+			return formatUrl(a.value()[0]);
 		}
-		if (url.equalsIgnoreCase("/")) {
-			return "/";
+		if (a.path().length > 0 && !a.path()[0].isEmpty()) {
+			return formatUrl(a.path()[0]);
 		}
-		if (url.endsWith("/")) {
-			url = url.substring(0, url.length() - 1);
+		return "";
+	}
+	//从 注解DeleteMapping 取得路径。 并确保它们是/开头（没有，则强加）
+	private String getUrlByMapping(DeleteMapping a) {
+		if (null != a.name() && !a.name().isEmpty()) {
+			return formatUrl(a.name());
 		}
+		if (a.value().length > 0 && !a.value()[0].isEmpty()) {
+			return formatUrl(a.value()[0]);
+		}
+		if (a.path().length > 0 && !a.path()[0].isEmpty()) {
+			return formatUrl(a.path()[0]);
+		}
+		return "";
+	}
+	//从 注解PutMapping 取得路径。 并确保它们是/开头（没有，则强加）
+	private String getUrlByMapping(PutMapping a) {
+		if (null != a.name() && !a.name().isEmpty()) {
+			return formatUrl(a.name());
+		}
+		if (a.value().length > 0 && !a.value()[0].isEmpty()) {
+			return formatUrl(a.value()[0]);
+		}
+		if (a.path().length > 0 && !a.path()[0].isEmpty()) {
+			return formatUrl(a.path()[0]);
+		}
+		return "";
+	}
+	//从 注解PostMapping 取得路径。 并确保它们是/开头（没有，则强加）
+	private String getUrlByMapping(PostMapping a) {
+		if (null != a.name() && !a.name().isEmpty()) {
+			return formatUrl(a.name());
+		}
+		if (a.value().length > 0 && !a.value()[0].isEmpty()) {
+			return formatUrl(a.value()[0]);
+		}
+		if (a.path().length > 0 && !a.path()[0].isEmpty()) {
+			return formatUrl(a.path()[0]);
+		}
+		return "";
+	}
+	//从 注解GetMapping 取得路径。 并确保它们是/开头（没有，则强加）
+	private String getUrlByMapping(GetMapping a) {
+		if (null != a.name() && !a.name().isEmpty()) {
+			return formatUrl(a.name());
+		}
+		if (a.value().length > 0 && !a.value()[0].isEmpty()) {
+			return formatUrl(a.value()[0]);
+		}
+		if (a.path().length > 0 && !a.path()[0].isEmpty()) {
+			return formatUrl(a.path()[0]);
+		}
+		return "";
+	}
+
+	//从 注解RequestMapping 取得路径。 并确保它们是/开头（没有，则强加）
+	private String getUrlByMapping(RequestMapping a) {
+		if (null != a.name() && !a.name().isEmpty()) {
+			return formatUrl(a.name());
+		}
+		if (a.value().length > 0 && !a.value()[0].isEmpty()) {
+			return formatUrl(a.value()[0]);
+		}
+		if (a.path().length > 0 && !a.path()[0].isEmpty()) {
+			return formatUrl(a.path()[0]);
+		}
+		return "";
+	}
+
+	//确保它们是/开头（没有，则强加）
+	private String formatUrl(String url) {
 		if (!url.startsWith("/")) {
-			url = "/" + url;
+			return "/" + url;
 		}
 		return url;
 	}
 
-//========== 解析SpringMVC注解的标签========================
-	private String getUrlByMapping(PatchMapping a) {
-		if (null != a.name() && !a.name().isEmpty()) {
-			return a.name();
-		}
-		if (a.value().length > 0 && !a.value()[0].isEmpty()) {
-			return a.value()[0];
-		}
-		if (a.path().length > 0 && !a.path()[0].isEmpty()) {
-			return a.path()[0];
-		}
-		return "";
-	}
-
-	private String getUrlByMapping(DeleteMapping a) {
-		if (null != a.name() && !a.name().isEmpty()) {
-			return a.name();
-		}
-		if (a.value().length > 0 && !a.value()[0].isEmpty()) {
-			return a.value()[0];
-		}
-		if (a.path().length > 0 && !a.path()[0].isEmpty()) {
-			return a.path()[0];
-		}
-		return "";
-	}
-
-	private String getUrlByMapping(PutMapping a) {
-		if (null != a.name() && !a.name().isEmpty()) {
-			return a.name();
-		}
-		if (a.value().length > 0 && !a.value()[0].isEmpty()) {
-			return a.value()[0];
-		}
-		if (a.path().length > 0 && !a.path()[0].isEmpty()) {
-			return a.path()[0];
-		}
-		return "";
-	}
-
-	private String getUrlByMapping(PostMapping a) {
-		if (null != a.name() && !a.name().isEmpty()) {
-			return a.name();
-		}
-		if (a.value().length > 0 && !a.value()[0].isEmpty()) {
-			return a.value()[0];
-		}
-		if (a.path().length > 0 && !a.path()[0].isEmpty()) {
-			return a.path()[0];
-		}
-		return "";
-	}
-
-	private String getUrlByMapping(GetMapping a) {
-		if (null != a.name() && !a.name().isEmpty()) {
-			return a.name();
-		}
-		if (a.value().length > 0 && !a.value()[0].isEmpty()) {
-			return a.value()[0];
-		}
-		if (a.path().length > 0 && !a.path()[0].isEmpty()) {
-			return a.path()[0];
-		}
-		return "";
-	}
-
-	private String getUrlByMapping(RequestMapping a) {
-		if (null != a.name() && !a.name().isEmpty()) {
-			return a.name();
-		}
-		if (a.value().length > 0 && !a.value()[0].isEmpty()) {
-			return a.value()[0];
-		}
-		if (a.path().length > 0 && !a.path()[0].isEmpty()) {
-			return a.path()[0];
-		}
-		return "";
-	}
 }
